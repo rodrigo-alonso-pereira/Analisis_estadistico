4ac57991cf1aa2963b570cd9df2d05a0147dc7b0
Rodrigo Pereira <rodrigo.pereira@usach.cl>
2024-07-12 19:21:28 -0400

    Folder PEP2

---
+++ PEP2/pep2_pregunta1.R
@@ -0,0 +1,16 @@
+#================
+# Primera pregunta (35 puntos)
+# Para su ramo de análisis de algoritmos y estructura de datos le solicitaron crear un algoritmo que sea
+# capaz de analizar bases de datos de una empresa con el objetivo de clasificar en percentiles los sueldos de
+# los empleados de manera gráfica y a través de un listado con cada nombre, sueldo y cuartil del empleado.
+# El día que presentaron los resultados, el profesor los organizo en parejas, les entrego los computadores de
+# la Universidad (todos con misma marca, modelo y año) y diversas bases de datos para analizar con sus
+# algoritmos elaborados. Los resultados de rendimiento de los algoritmos (suyo y de su pareja) fueron los
+# siguientes:
+#================
+
+#================
+# a) Genere en R una representación para los datos entregados, calcule los parámetros básicos (media,
+#    desviación estándar y coeficientes de variación) y explique a que conclusión puede llegar con los
+#    resultados obtenidos. (10 puntos).
+#================
